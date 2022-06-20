23d46ce2cea829b0694411c79316865c4bcf656e
fajarAriPrasetya <107893727+fajarAriPrasetya@users.noreply.github.com>
2022-06-21 06:26:49 +0700

    Add files via upload
    @dedigun

---
+++ home.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php
+include "kon.php";
+?>
+<head>
+<title>CV. A</title>
+	<style type="text/css">
+			header, section, footer, aside, nav, article, figure, figcaption {
+				display: block;}
+			body {
+				color: #666666;
+				background-color: #f9f8f6;
+				background-image: url("aa.jpg");
+				background-position: center;
+				height: 600px;
+			  	weight: 200px;
+		</style>
+</head>
+<body> <form method="POST" action="logout.php">
+	<body>
+		<div class="wrapper">
+			<header>
+			<br/>
+			<br/>
+			<br/>
+			<br/>
+			<br/>
+			<br/>
+			<br/>			
+				<h1><center><font color="white">CV. A</font></center></h1>
+				<br/>
+				<table border="5" align="center" width="700" height="100">
+				<tr>
+					<td><a href="merk_i.php"><font color="white">MERK MESIN JAHIT</font></a></td>
+					<td><a href="jenis_i.php"><font color="white">JENIS MESIN JAHIT</font></a></td>
+					<td><a href="mesin_i.php"><font color="white">MESIN JAHIT</font></a></td>
+					<td><a href="detailMesin_i.php"><font color="white">DETAIL MESIN JAHIT</font></a></td>
+                    <td><a href="depresiasi.php"><font color="white">DEPRESIASI</font></a></td>
+                    <td><a href="depresiasi_t.php"><font color="white">LAPORAN</font></a></td>
+				</tr>			
+				</table><br/><br/>
+				<center><button type="submit" value="keluar">Logout</button></center>
+				<br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
+			<footer>
+			<p><h3><font color="white">Copyright@ Apriliana 2018</font></h3></p>
+			</footer>
+			</header>
+		</div><!-- .wrapper -->
+	</body>
+</html>
